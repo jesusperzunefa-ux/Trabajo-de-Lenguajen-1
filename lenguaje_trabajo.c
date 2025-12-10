98a0573bdd07e6664a09af78792198fdaeb58b4d
Santiago Acosta <Thgo0312@gmail.com>
2025-12-10 01:54:51 -0400

    agregado: -agregue dos librerias y se agrego un sistema aleaorio para numeros del 1 al 10 -se agrego un mensaje al final -agregue el switch para seleccionar la opcion de personaje basandose en numero aleatorio

--- lenguaje_trabajo.c
+++ lenguaje_trabajo.c
@@ -3,9 +3,14 @@
 
 /*Seleccion de que fase eres de dragon ball*/
 #include <stdio.h>
+#include <stdlib.h>  
+#include <time.h>
 int main(){
     int seleccion_si_no=0;
     int validacion=0;
+
+srand(time(NULL));
+
 printf("NOTA: si no seleccionas ninguna de las opciones se cerrara el programa\n\n");
 printf("Bienvenido, quiere saber que personaje de dragon ball eres?\n1. si\n2. no\n");
 validacion = scanf("%i",&seleccion_si_no);
@@ -17,11 +22,53 @@ if (validacion != 1 || seleccion_si_no < 1 || seleccion_si_no > 2)
 if (seleccion_si_no == 1)
 {
 
+    int personaje_id = (rand() % 10) + 1; // Genera un número aleatorio entre 1 y 10
+
+        printf("\n¡Analizando tu personaje...!\n");
+
+        switch (personaje_id)
+        {
+            case 1:
+                printf("Eres: ¡Goku¡ un sayayin que nunca deja de entrenar ni de comer .\n");
+                break;
+            case 2:
+                printf("Eres: ¡Vegeta! orgulloso, fuerte y siempre buscando mejorar.\n");
+                break;
+            case 3:
+                printf("Eres: ¡Piccolo! serio, sabio y un buen estratega.\n");
+                break;
+            case 4:
+                printf("Eres: ¡Gohan! tranquilo pero con un gran poder oculto.\n");
+                break;
+            case 5:
+                printf("Eres: ¡Krilin! Leal y valiente, el mejor amigo de Goku.\n");
+                break;
+            case 6:
+                printf("Eres: ¡Trunks del Futuro! determinado y noble.\n");
+                break;
+            case 7:
+                printf("Eres: ¡Freezer! Elegante, cruel y con una ambición ilimitada.\n");
+                break;
+            case 8:
+                printf("Eres: ¡Broly! Un poder legendario difícil de controlar.\n");
+                break;
+            case 9:
+                printf("Eres: ¡Bulma! Inteligente y una genio de la tecnología.\n");
+                break;
+            case 10:
+                printf("Eres: ¡Maestro Roshi! Viejo pero sabio, con técnicas clásicas y mucho carisma.\n");
+                break;
+            default:
+                // Esto no debería pasar, pero es una buena práctica
+                printf("Error de cálculo. Parece que eres un personaje no canónico.\n");
+                break;
+        }
+
 } else if (seleccion_si_no == 2)
 {
     printf("Esta bien :(\nCuando quieras puedes volver\n");
 }
-
+    printf("Recuerda incluso los mas debiles pueden sorprender");
 
 return 0;
 }
