be6b2ae80f804d286a534973fd0741dfb43d4a3b
jess <jesusperzunefa@gmail.com>
2025-12-10 00:51:24 -0400

    Comienzo del programa -Agregaron una pregunta con un printf y tambien una nota -Un scanf con su validacion, y las variable que se usaron en ambas -Una estrucura condicional de la pregunta

--- lenguaje_trabajo.c
+++ lenguaje_trabajo.c
@@ -1,2 +1,27 @@
 //Jesus Perez 32.025.632 (jess) 
 //Santiago Acosta 32.016.516
+
+/*Seleccion de que fase eres de dragon ball*/
+#include <stdio.h>
+int main(){
+    int seleccion_si_no=0;
+    int validacion=0;
+printf("NOTA: si no seleccionas ninguna de las opciones se cerrara el programa\n\n");
+printf("Bienvenido, quiere saber que personaje de dragon ball eres?\n1. si\n2. no\n");
+validacion = scanf("%i",&seleccion_si_no);
+if (validacion != 1 || seleccion_si_no < 1 || seleccion_si_no > 2)
+{
+    printf("La proxima pon una de las opciones que se indicaron -_-");
+    return 1;
+}
+if (seleccion_si_no == 1)
+{
+
+} else if (seleccion_si_no == 2)
+{
+    printf("Esta bien :(\nCuando quieras puedes volver\n");
+}
+
+
+return 0;
+}
